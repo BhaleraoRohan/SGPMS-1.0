d9e1d5fef701b858931752bc33bb243abfefe70b
Rohan <bhalerao.rohan28@gmail.com>
2019-06-30 15:42:17 +0530

    Basic UI setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule,Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http';
+
+
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { SidenavComponent } from './layout/components/sidenav/sidenav.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const appRoutes : Routes =[
+  {
+    path : 'dashboard',component : DashboardComponent
+  }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidenavComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing : true}
+    ),
+    BrowserModule,
+    HttpClientModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
